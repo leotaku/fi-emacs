80565893397bede3c4e9b64ea266239660de9aa7
Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
2020-06-11 03:10:56 +0200

    Change fi.el to only include fi-prefixed files

--- fi.el
+++ fi.el
@@ -1,14 +1,32 @@
-;;; fi.el --- Meta-package for fi-emacs -*- lexical-binding: t; -*-
+;;; fi.el --- Various utilities for fi-emacs -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2019-2020 Leo Gaskin
+
+;; Author: Leo Gaskin <leo.gaskin@brg-feldkirchen.at>
+;; Created: 19 July 2019
+;; Homepage: https://github.com/leotaku/fi-emacs
+;; Keywords: fi-emacs, configuration, extension, lisp
+;; Package-Version: 0.1.0
+;; Package-Requires: ((emacs "25.1"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
 ;; This meta-package collectively loads the various facilities
-;; provided by all packages in fi-emacs.  Note that you can also
-;; install and load the individual packages separately.
+;; provided by all fi-prefixed packages in the fi-emacs repository.
 
-(require 'bk)
-(require 'sd)
-(require 'sd-display)
 (require 'fi-config)
 (require 'fi-helpers)
 
