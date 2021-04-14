40b7ebaf24c8e5d7353bf2f059361442e38999ce
Leo Gaskin <leo.gaskin@le0.gs>
2021-04-14 20:00:59 +0200

    Add explicit leaf.el dependency to bk.el

--- bk.el
+++ bk.el
@@ -7,7 +7,7 @@
 ;; Homepage: https://github.com/leotaku/fi-emacs
 ;; Keywords: fi-emacs, configuration, lisp
 ;; Package-Version: 0.1.0
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (leaf "4.4"))
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -32,6 +32,7 @@
 
 (require 'sd)
 (require 'fi-config)
+(require 'leaf)
 
 ;;; Code:
 
